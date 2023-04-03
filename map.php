7a3ea61cd2c504d31baf202991ac6d97c29cf9f1
kemeson <kemeson123@gmail.com>
2023-04-03 07:52:41 -0400

    planet api 13

--- map.php
+++ map.php
@@ -1023,7 +1023,7 @@ include('verifica_login.php')
         }        
 
         var planet = L.tileLayer(
-        'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2023_02_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK647ba0a2c0ee450a967d3f8fdce806bd',
+        'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2023_02_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK690ab731a2f44e3198f12dab18b5983a',
         {
           id: 'global_monthly',
         });
