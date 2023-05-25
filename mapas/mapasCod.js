83dbbc9fb878a2dd382d3646baea6a363aae4a16
kemeson <kemeson123@gmail.com>
2023-05-25 07:40:19 -0400

    planet 04 api 25/05

--- mapas/mapasCod.js
+++ mapas/mapasCod.js
@@ -112,7 +112,7 @@
         }
         
         var planet = L.tileLayer(
-        'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2023_04_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK1da38a615653413d8fd1051ba401fa70',
+        'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2023_04_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAKbd22ddcc8690482e84d715d243b1b78a',
         {
           id: 'global_monthly',
         });
