f29efefc80cb2bdf3bde880c7f12e9c61f964332
Kemeson <kemeson123@gmail.com>
2023-10-19 08:39:28 -0300

    planet api 19/10

--- mapas/mapasCod.js
+++ mapas/mapasCod.js
@@ -117,7 +117,7 @@
           groupLayPlanet.clearLayers();
           
           var planet = L.tileLayer(
-            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK6ea1f8fd6743429697b5c79bed1b398d',
+            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK2b7bed68a4854d7a9adbe0656971fa83',
             {
               id: 'global_monthly',
             });
