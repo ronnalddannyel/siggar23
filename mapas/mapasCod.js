5135d404d0547d3a54c45d19dfa047cf1360f556
Kemeson <kemeson123@gmail.com>
2023-11-22 08:43:31 -0300

    planet api 22/11

--- mapas/mapasCod.js
+++ mapas/mapasCod.js
@@ -117,7 +117,7 @@
           groupLayPlanet.clearLayers();
           
           var planet = L.tileLayer(
-            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK2083bbf6e35e4839ba39767c1f40ed2f',
+            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAKdacc89bd1c614bb6b775023df6421c9e',
             {
               id: 'global_monthly',
             });
