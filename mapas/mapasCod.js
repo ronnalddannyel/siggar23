501ad383f2c565ef4cddd3e6e7bed2c781ed9123
kemeson <kemeson123@gmail.com>
2024-02-01 02:54:23 -0400

    camadas e planet att

--- mapas/mapasCod.js
+++ mapas/mapasCod.js
@@ -117,7 +117,7 @@
           groupLayPlanet.clearLayers();
           
           var planet = L.tileLayer(
-            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK88a5b4a48b984269b2c463ae59cd5ce4',
+            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAKd6ebb11e8d83461a8830b404262e1726',
             {
               id: 'global_monthly',
             });
