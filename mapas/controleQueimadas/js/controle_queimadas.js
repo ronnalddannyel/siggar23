e7e987f80e8f05e2e6fb397295aa4be07b0f3efa
Kemeson <kemeson123@gmail.com>
2024-03-27 13:37:31 -0300

    queimadas

--- mapas/controleQueimadas/js/controle_queimadas.js
+++ mapas/controleQueimadas/js/controle_queimadas.js
@@ -15,7 +15,7 @@ function controleQueimadas(el){
   
   var controleQueimada = L.geoJson(data, {
     filter: function (features) {
-      var date = new Date(features.properties.data_f);
+      var date = new Date(features.properties.data_i);
       
         if(date.getUTCFullYear() == '2023'){
           cont++;
@@ -26,7 +26,7 @@ function controleQueimadas(el){
       return L.marker(latlng, {icon: contFogo})
     },
     onEachFeature: function (geom, layer) {
-      layer.bindPopup('<h6><b>Área Licenciada para Uso do Fogo - 2023</b></h6><p><b>Denominação:</b> '+geom.properties.denomina+'<br><b>Endereço:</b> '+geom.properties.endereco+'<br><b>Município:</b> '+geom.properties.municipio+'<br><b>Instituição:</b> '+geom.properties.instituto+'<br><b>Latitude:</b> '+geom.properties.latitude+'<br><b>Longitude:</b> '+geom.properties.longitude+'<br><b>Atividade da Queimada:</b> '+geom.properties.atividade+'<br><b>Área Licenciada:</b> '+geom.properties.area_lic.toLocaleString('pt-BR', {minimumFractionDigits: 4,maximumFractionDigits: 4})+' ha.<br><b>Data Final:</b> '+geom.properties.data_f+'<br><b>N° do Processo (SEI):</b> '+geom.properties.proces_sei+'</p>');
+      layer.bindPopup('<h6><b>Área Licenciada para Uso do Fogo - 2023</b></h6><p><b>Denominação:</b> '+geom.properties.denomina+'<br><b>Endereço:</b> '+geom.properties.endereco+'<br><b>Município:</b> '+geom.properties.municipio+'<br><b>Instituição:</b> '+geom.properties.instituto+'<br><b>Latitude:</b> '+geom.properties.latitude+'<br><b>Longitude:</b> '+geom.properties.longitude+'<br><b>Atividade da Queimada:</b> '+geom.properties.atividade+'<br><b>Área Licenciada:</b> '+geom.properties.area_lic.toLocaleString('pt-BR', {minimumFractionDigits: 4,maximumFractionDigits: 4})+' ha.<br><b>Data Final:</b> '+geom.properties.data_f+'<br><b>Data Início:</b> '+geom.properties.data_i+'<br><b>Número de Autorização:</b> '+geom.properties.n_auto+'<br><b>N° do Processo (SEI):</b> '+geom.properties.proces_sei+'</p>');
     },
     });
     groupLayControleQueimada.clearLayers();
@@ -106,7 +106,7 @@ function controleQueimadas2(el){
   
   var controleQueimada = L.geoJson(data, {
     filter: function (features) {
-      var date = new Date(features.properties.data_f);
+      var date = new Date(features.properties.data_i);
       
         if(date.getUTCFullYear() == '2024'){
           cont++;
@@ -117,7 +117,7 @@ function controleQueimadas2(el){
       return L.marker(latlng, {icon: contFogo})
     },
     onEachFeature: function (geom, layer) {
-      layer.bindPopup('<h6><b>Área Licenciada para Uso do Fogo - 2024</b></h6><p><b>Denominação:</b> '+geom.properties.denomina+'<br><b>Endereço:</b> '+geom.properties.endereco+'<br><b>Município:</b> '+geom.properties.municipio+'<br><b>Instituição:</b> '+geom.properties.instituto+'<br><b>Latitude:</b> '+geom.properties.latitude+'<br><b>Longitude:</b> '+geom.properties.longitude+'<br><b>Atividade da Queimada:</b> '+geom.properties.atividade+'<br><b>Área Licenciada:</b> '+geom.properties.area_lic.toLocaleString('pt-BR', {minimumFractionDigits: 4,maximumFractionDigits: 4})+' ha.<br><b>Data Final:</b> '+geom.properties.data_f+'<br><b>N° do Processo (SEI):</b> '+geom.properties.proces_sei+'</p>');
+      layer.bindPopup('<h6><b>Área Licenciada para Uso do Fogo - 2024</b></h6><p><b>Denominação:</b> '+geom.properties.denomina+'<br><b>Endereço:</b> '+geom.properties.endereco+'<br><b>Município:</b> '+geom.properties.municipio+'<br><b>Instituição:</b> '+geom.properties.instituto+'<br><b>Latitude:</b> '+geom.properties.latitude+'<br><b>Longitude:</b> '+geom.properties.longitude+'<br><b>Atividade da Queimada:</b> '+geom.properties.atividade+'<br><b>Área Licenciada:</b> '+geom.properties.area_lic.toLocaleString('pt-BR', {minimumFractionDigits: 4,maximumFractionDigits: 4})+' ha.<br><b>Data Final:</b> '+geom.properties.data_f+'<br><b>Data Início:</b> '+geom.properties.data_i+'<br><b>Número de Autorização:</b> '+geom.properties.n_auto+'<br><b>N° do Processo (SEI):</b> '+geom.properties.proces_sei+'</p>');
     },
     });
     groupLayControleQueimada2.clearLayers();
