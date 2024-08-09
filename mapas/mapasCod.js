6ce1ef33d56714f49719fd83c7fd971806a2cf7e
Kemeson <117468978+Kemeson@users.noreply.github.com>
2024-08-09 07:02:25 -0400

    Update mapasCod.js
    planet api

--- mapas/mapasCod.js
+++ mapas/mapasCod.js
@@ -117,7 +117,7 @@
           groupLayPlanet.clearLayers();
           
           var planet = L.tileLayer(
-            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK02852a52498c4ddaa712c77cb61ff6fa',
+            'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_'+ano+'_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK8d522b1b45c14868950b30ec9e1cba39',
             {
               id: 'global_monthly',
             });
